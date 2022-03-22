e8a8b27fe54a327c2fcae6ffa6b526b58ee8df5f
Luis Sandoval <lfsandovalruiz@outlook.com>
2022-03-21 22:02:05 -0600

    add: pasteleria

---
+++ FrontEnd-Mission/05-pasteleria/src/views/Inicio.vue
@@ -0,0 +1,23 @@
+<template>
+  <Header />
+  <Navegacion />
+  <Contenido />
+</template>
+
+<script>
+
+import Header from '@/components/Header.vue'
+import Navegacion from '@/components/Navegacion.vue'
+import Contenido from '@/components/Contenido.vue'
+
+export default {
+  name: "Inicio",
+  components: {
+    Header,
+    Navegacion,
+    Contenido
+  }
+
+}
+
+</script>
